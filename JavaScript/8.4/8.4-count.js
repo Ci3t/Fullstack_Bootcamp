c250382242e1d14863fbd518ad885594f2d1ab8b
Ci3t <ranigfx@gmail.com>
2022-09-21 23:35:15 +0300

    JS BC

--- JavaScript/8.4/8.4-count.js
+++ JavaScript/8.4/8.4-count.js
@@ -1,23 +1,35 @@
 const arr = ["Hello", "Good Day", "Your Welcome", "hotdog", "hamburgers"];
 const count = (array) => {
-    let arr = {}
-    let arr2 = {}
-    array.forEach(w => w.split('').forEach(e => arr[e] = (arr[e] || 0)+1));
-
-    Object.entries(arr).forEach(element =>{
-        let key = element[0];
-        let value = element[1];
-        arr2[key] = value;
+    let arr2 = array.toString().toLowerCase().split('').reduce((ar,i)=>{
+        if(ar[i]){
+          ar[i]++
+        } else {
+          ar[i]=1
+        }
+        return ar
+      },{})
+      return console.log(arr2);
+};
     
-    })
+    //! one way
+    // let arr = {}
+    // let arr2 = {}
+    //  array.forEach(w => w.split('').forEach(e => arr[e] = (arr[e] || 0)+1));
+     
 
-console.log(arr2)
-  } 
+    // Object.entries(arr).forEach(element =>{
+    //     let key = element[0];
+    //     let value = element[1];
+    //     arr2[key] = value;
+    
+    // })
+// console.log(arr2)
+//   } 
 
 //    for (arr of array){
 //     arr2 = arr.split('');
 //    }
-    console.log(arr)
+    // console.log(arr)
 
 
 count(arr)
